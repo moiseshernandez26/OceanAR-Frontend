2b32d07fae9c20dcb36f3b4ec47979d6ec66d529
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 16:52:31 -0500

    Se crean las rutas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { lazy, Suspense, useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AppContext, AppContextProvider } from './context/state'
 import './App.css'
+import './themes/variables.css'
+import Loading from './components/Loading'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import('./views/Home'))
+const SignIn = lazy(() => import('./views/SignIn'))
+const SignUp = lazy(() => import('./views/SignUp'))
+
+function ARPlacesAPP () {
+  const { state } = useContext(AppContext)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <Router>
+        <Suspense fallback={<Loading />}>
+          {state.isLoggedin
+            ? (
+              <Routes>
+                <Route path='/home' element={<Home />} />
+                <Route path='*' element={<Home />} />
+              </Routes>
+              )
+            : (
+              <Routes>
+                <Route path='/sign-in' element={<SignIn />} />
+                <Route path='/sign-up' element={<SignUp />} />
+                <Route path='*' element={<SignIn />} />
+              </Routes>
+              )}
+        </Suspense>
+      </Router>
     </div>
   )
 }
 
+const App = () => {
+  return (
+    <AppContextProvider>
+      <ARPlacesAPP />
+    </AppContextProvider>
+  )
+}
 export default App
