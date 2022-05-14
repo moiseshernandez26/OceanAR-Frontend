1a54c09edc36d5e45c5f4fda1c3efaece0b2cc3d
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 16:55:50 -0500

    Se crean los servicios para el usuario

---
+++ src/services/auth.service.js
@@ -0,0 +1,31 @@
+import server from '../api/server'
+export const createNewAccount = async (data) => {
+  try {
+    const res = await server.post('/user', JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    return res.data
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
+export const logIn = async (data) => {
+  try {
+    const res = await server.post(
+      '/user/auth/login',
+      JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    return res.data
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
