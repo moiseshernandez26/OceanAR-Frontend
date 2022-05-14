4e14dfec667ff6743c93c9fbf6336bfa4e60cb15
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 18:58:56 -0500

    mock cities

---
+++ src/mocks/cities.js
@@ -0,0 +1,8 @@
+export const cities = [
+  {
+    id: '1',
+    name: 'New York',
+    urlImage: 'https://media.admagazine.com/photos/61e5acc706c10ae95c71b902/2:3/w_1123,h_1685,c_limit/New-York-skyline.jpg',
+    urlMap: 'iframe'
+  }
+]
