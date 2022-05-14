f055a287a3ed7acefde3f44aba03105eaacfc23d
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 18:32:23 -0500

    Componente hijo para el login

---
+++ src/components/LogInForm/index.jsx
@@ -0,0 +1,81 @@
+import { TextField, Grid, Button } from '@mui/material'
+import { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../../context/state'
+import { logIn } from '../../services/auth.service'
+import Loading from '../../components/Loading'
+
+import './styles.css'
+
+const LogInForm = () => {
+  const { dispatch } = useContext(AppContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const redirectRegister = () => {
+    navigate('/sign-up')
+  }
+
+  const onChangeEmail = ({ target: { value } }) => {
+    setEmail(value)
+  }
+
+  const onChangePassword = ({ target: { value } }) => {
+    setPassword(value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    const data = {
+      email,
+      password
+    }
+    try {
+      const { token, user } = await logIn(data)
+      dispatch({ type: 'set-is-loggedin', isLoggedin: true })
+      dispatch({ type: 'set-token', token })
+      dispatch({ type: 'set-user', user })
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      <div className='log-in-container'>
+        <form onSubmit={handleSubmit}>
+          <div className='wrapper__logo'>
+            <img src='./logo_ar.png' alt='logo' className='logo' />
+          </div>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField label='Correo' variant='standard' type='Email' onChange={onChangeEmail} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label='Contraseña' variant='standard' type='Password' onChange={onChangePassword} />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant='contained' type='submit'>
+                Iniciar Sesión
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <p>¿No tienes una cuenta?</p>
+            </Grid>
+            <Grid item xs={12} style={{ paddingBottom: '1em' }}>
+              <Button variant='outlined' onClick={redirectRegister}>Crear una cuenta</Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default LogInForm
