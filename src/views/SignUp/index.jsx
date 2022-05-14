65bfef1ac9ca6da0bfed690d99dc19da05d7e00c
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 18:33:22 -0500

    Se crea vista sign up

---
+++ src/views/SignUp/index.jsx
@@ -0,0 +1,12 @@
+import SignUpForm from '../../components/SignUpForm'
+import './styles.css'
+
+const SignUp = () => {
+  return (
+    <div className='container-view-singup'>
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default SignUp
