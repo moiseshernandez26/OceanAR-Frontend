9cb1da2ff4b5969ad88522ae3858b106a50b84f0
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 18:33:06 -0500

    Se crea vista sign in

---
+++ src/views/SignIn/index.jsx
@@ -0,0 +1,12 @@
+import LogInForm from '../../components/LogInForm'
+import './styles.css'
+
+const SignIn = () => {
+  return (
+    <div className='container-view-singin'>
+      <LogInForm />
+    </div>
+  )
+}
+
+export default SignIn
