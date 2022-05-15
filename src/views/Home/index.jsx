b72d95a392049d5bca53bbcef2838b390ac7f3a9
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-15 15:10:42 -0500

    se consulta el endpoint de cities

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,26 +1,44 @@
 import { Container, Grid } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import AppBarAR from '../../components/AppBarAR'
 import CardAR from '../../components/CardAR'
+import Loading from '../../components/Loading'
 
-import { cities } from '../../mocks/cities'
-import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../../context/state'
-import { useContext } from 'react'
+import { getCities } from '../../services/city.service'
 
 const Home = () => {
   const { dispatch } = useContext(AppContext)
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    requestCities()
+  }, [])
+
+  const requestCities = async () => {
+    setIsLoading(true)
+    const response = await getCities()
+    setCities(response)
+    setIsLoading(false)
+  }
+
   const navigateToPlace = (place) => {
     dispatch({
       type: 'set-url-map',
       urlMap: place.urlMap
     })
-    navigate(`/animal/${place.id}`)
+    navigate(`/animal/${place.Id}`)
   }
 
   const navigate = useNavigate()
-
+  console.log(cities)
   return (
     <>
+      {isLoading && <Loading />}
+
       <AppBarAR component='home' />
       <Container maxWidth='sm' style={{ paddingTop: '2em' }}>
         <Grid container spacing={2}>
