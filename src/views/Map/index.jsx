506ec7d3761c6847e9e30e1a77a3cfbbbd67b823
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 20:41:16 -0500

    Vista maps para mostrar la ubicación del lugar

--- src/views/Map/index.jsx
+++ src/views/Map/index.jsx
@@ -1,8 +1,27 @@
-const Map = ({ urlMap }) => {
+import { Container } from '@mui/material'
+import { useContext } from 'react'
+import AppBarAR from '../../components/AppBarAR'
+import { AppContext } from '../../context/state'
+
+import './styles.css'
+const Map = () => {
+  const { state } = useContext(AppContext)
   return (
-    <div className='map'>
-      <iframe src={urlMap} width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade' />
-    </div>
+    <>
+      <AppBarAR component='map' />
+      <Container maxWidth='sm' style={{ paddingTop: '2em' }}>
+        <div className='map'>
+          <iframe
+            className='map-iframe'
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.305935303!2d-74.25986548248684!3d40.69714941932609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNueva%20York%2C%20EE.%20UU.!5e0!3m2!1ses-419!2smx!4v1652574989759!5m2!1ses-419!2smx'
+            style={{ border: '0' }}
+            allowfullscreen=''
+            loding='lazy'
+            referrerpolicy='no-referrer-when-downgrade'
+          />
+        </div>
+      </Container>
+    </>
   )
 }
 
