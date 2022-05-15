ee0d09dcaf5f6a69ccd6107a79b0de9057e66304
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 19:01:30 -0500

    Se modifica el home para iniciar su diseño

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,8 +1,28 @@
+import { Container, Grid } from '@mui/material'
+import AppBarAR from '../../components/AppBarAR'
+import CardAR from '../../components/CardAR'
+
+import { cities } from '../../mocks/cities'
+import { useNavigate } from 'react-router-dom'
+
 const Home = () => {
+  const navigateToPlace = (place) => {
+    navigate(`/animal/${place.id}`)
+  }
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <AppBarAR component='home' />
+      <Container maxWidth='sm' style={{ paddingTop: '2em' }}>
+        <Grid container spacing={2}>
+          {cities.map((city, index) => (
+            <CardAR key={index} place={city} handleClick={navigateToPlace} />
+          ))}
+        </Grid>
+      </Container>
+    </>
   )
 }
 
