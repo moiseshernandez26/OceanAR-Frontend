47f00979f124852d8e8e66ad19c221c6530af880
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-15 15:12:31 -0500

    Se quita mock y se consulta a la api

--- src/views/Description/index.jsx
+++ src/views/Description/index.jsx
@@ -4,22 +4,38 @@ import { useParams } from 'react-router-dom'
 import PreviewIcon from '@mui/icons-material/Preview'
 
 import AppBarAR from '../../components/AppBarAR'
+import Loading from '../../components/Loading'
+
 import { animals } from '../../mocks/animals'
 
 import './styles.css'
+import { useEffect, useState } from 'react'
+import { getAnimalByIdService } from '../../services/animals.service'
 
 const Description = () => {
+  const [animal, setAnimal] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
   const params = useParams()
 
-  const animalsFiltered = animals.find(animal => animal.idCity === +params.id)
+  useEffect(() => {
+    requestAnimal()
+  }, [])
+
+  const requestAnimal = async () => {
+    setIsLoading(true)
+    const response = await getAnimalByIdService(params.id)
+    setAnimal(response.data)
+    setIsLoading(false)
+  }
 
   return (
     <>
+      {isLoading && <Loading />}
       <AppBarAR component='description' />
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <img
-            src={animalsFiltered.urlImage}
+            src={animal?.UrlImage}
             alt='item'
             style={{ width: '100%', height: 'auto', objectFit: 'cover', maxHeight: '50vh' }}
           />
@@ -28,16 +44,16 @@ const Description = () => {
           <Container maxWidth='sm'>
             <div className='wrapper_btn'>
               <Button startIcon={<PreviewIcon />}>
-                <a href='/axolotl.html' className='link-ar'>
+                <a href={`/${animal?.FileName}/index.html`} className='link-ar'>
                   Preview
                 </a>
               </Button>
             </div>
-            <Typography variant='h5' component='h2'>
-              {animalsFiltered.name}
+            <Typography variant='h5' component='h2' style={{ paddingTop: '1em' }}>
+              {animal?.Name}
             </Typography>
-            <Typography variant='body2' component='p'>
-              {animalsFiltered.description}
+            <Typography variant='body2' component='p' style={{ paddingTop: '1em' }}>
+              {animal?.Description}
             </Typography>
           </Container>
         </Grid>
