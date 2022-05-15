52b48a3d2128c112d7b8c2dc6a46c1a6c990a939
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 20:40:25 -0500

    Se anexan mocks para pruebas

---
+++ src/mocks/animals.js
@@ -0,0 +1,9 @@
+export const animals = [
+  {
+    id: '1',
+    name: 'Ajolote',
+    urlImage: 'https://i.natgeofe.com/n/de94c416-6d23-45f5-9708-e8d56289268e/naturepl_01132178.jpg?w=636&h=631',
+    idCity: 1,
+    description: 'Lorem ipsum'
+  }
+]
