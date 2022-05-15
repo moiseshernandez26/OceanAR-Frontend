a2139fced9fc4133e7d6e8ac93509377a6f37e09
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 20:41:51 -0500

    Se añade funcionalidad de la vista animal

--- src/views/Animal/index.jsx
+++ src/views/Animal/index.jsx
@@ -1,11 +1,52 @@
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Container, Grid, Box, Button } from '@mui/material'
+import DirectionsIcon from '@mui/icons-material/Directions'
+
+import AppBarAR from '../../components/AppBarAR'
+import CardAR from '../../components/CardAR'
+import { animals } from '../../mocks/animals'
 const Animal = () => {
   const params = useParams()
+  const navigate = useNavigate()
+
+  const navigateToPlace = (place) => {
+    navigate(`/description/${place.id}`)
+  }
+
+  const animalsFiltered = animals.filter(animal => animal.idCity === +params.id)
+
+  const handleClick = () => {
+    navigate('/map')
+  }
+
   return (
-    <div>
-      <h1>Animals</h1>
-      {params.id}
-    </div>
+    <>
+      <AppBarAR component='home' />
+      <Container maxWidth='sm' style={{ paddingTop: '2em' }}>
+        <Grid container style={{ paddingBottom: '2em' }}>
+          <Grid item xs={6}>
+            <Box
+              className='box-item'
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
+            >
+              <Button
+                startIcon={<DirectionsIcon />}
+                onClick={handleClick}
+              >
+                Maps
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          {animalsFiltered.map((city, index) => (
+            <CardAR key={index} place={city} handleClick={navigateToPlace} />
+          ))}
+        </Grid>
+      </Container>
+    </>
   )
 }
 
