4153a680bd5af093a2a1ed14de2308b9a38b599e
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 19:02:39 -0500

    Se crea el app bar para mostrar el titulo

---
+++ src/components/AppBarAR/index.jsx
@@ -0,0 +1,40 @@
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useNavigate } from 'react-router-dom'
+
+export default function AppBarAR ({ component }) {
+  const navigate = useNavigate()
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static' style={{ background: '#000' }}>
+          <Toolbar>
+            <IconButton
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+              sx={{ mr: 2 }}
+              onClick={() => {
+                if (component === 'map') {
+                  navigate(-1)
+                }
+                navigate('/')
+              }}
+            >
+              {component === 'home' ? <MenuIcon /> : <ArrowBackIcon />}
+            </IconButton>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              AR Places
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  )
+}
